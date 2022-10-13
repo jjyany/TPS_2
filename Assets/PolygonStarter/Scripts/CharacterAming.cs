558c21be995691d7f23e6c270e12a566ac7e1a81
jihwanang <ji.hwanang@gmail.com>
2022-10-14 00:00:19 +0900

    TPS_첫날

---
+++ Assets/PolygonStarter/Scripts/CharacterAming.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAming : MonoBehaviour
+{
+    public float turnSpeed = 15f;
+    Camera mainCam;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    void Update()
+    {
+        float yawCamera = mainCam.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/PolygonStarter/Scripts/CharacterMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Animator anim;
+    private Vector2 input;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+
+        anim.SetFloat("InputX", input.x);
+        anim.SetFloat("InputY", input.y);
+    }
+}
